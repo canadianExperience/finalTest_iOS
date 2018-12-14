0d3a4d4b3929e4b96b49784430382dc2a672bcfb
canadianExperience <lena2157@mail.ru>
2018-12-13 19:23:59 -0500

    Initial Commit

---
+++ bmi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bmi
+//
+//  Created by Elena Melnikova on 2018-12-13.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
